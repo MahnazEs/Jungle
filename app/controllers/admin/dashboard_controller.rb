bcd7ab78dcf059b284a34efbe734ae4b136da3b5
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-04-08 20:00:30 -0700

    change admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+  #http_basic_authenticate_with name: ENV["ADMIN_LOGIN"], password: ENV["ADMIN_PASS"]
+  http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
